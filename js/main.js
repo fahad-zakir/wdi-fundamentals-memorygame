6a74ff1ee808722385f7453d4a3f544d6abdd06c
Fahad Zakir <abumusa1981@hotmail.com>
2017-11-20 00:30:11 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push('cardOne');
+var cardTwo = cards[3];
+cardsInPlay.push('cardTwo');
+console.log("User flipped queen");
+console.log("User flipped king");
 
-console.log("User flipped " + cardOne);
+
+if (cardsInPlay[0] === cardsInPlay[1]){
+alert("You found a match!");
+} else {
+alert("Sorry, try again");
+}
+
